2195bd50085309bbb4aa87ae3db454a11a8020cf
jmolinafi18dw <ajuanenama@zubirimanteo.com>
2019-11-25 11:36:12 +0100

    Diseño establecido

---
+++ resources/views/index.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>GastroSociety</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/color/default.css" rel="stylesheet">
+	<link rel="shortcut icon" href="assets/img/favicon.ico">
+	<!-- =======================================================
+    Theme Name: Maxim
+    Theme URL: https://bootstrapmade.com/maxim-free-onepage-bootstrap-theme/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+	======================================================= -->
+</head>
+
+<body>
+	<!-- navbar -->
+	<div class="navbar-wrapper">
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<!-- Responsive navbar -->
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</a>
+					<h1 class="brand"><a href="/">GastroSociety</a></h1>
+					<!-- navigation -->
+					<nav class="pull-right nav-collapse collapse">
+						<ul id="menu-main" class="nav">
+							<li><a title="team" href="#about">Sobre nosotros</a></li>
+							<li><a title="services" href="#blog">Top Sociedades</a></li>
+							<li><a title="contact" href="#contact">Contacto</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Header area -->
+	<div id="header-wrapper" class="header-slider">
+		<header class="clearfix">
+			<div class="logo">
+				<img src="assets/img/logo-image.png" alt="" />
+			</div>
+		</header>
+	</div>
+	<!-- spacer section -->
+	<section class="spacer green">
+		<div class="container">
+			<div class="row">
+				<div class="span6 alignright flyLeft">
+					<blockquote class="large">Solo reserva, tus amigos harán el resto...<cite>GastroSociety</cite>
+					</blockquote>
+				</div>
+				<div class="span6 aligncenter flyRight">
+					<i class="icon-beer icon-10x"></i>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end spacer section -->
+	<!-- section: team -->
+	<section id="about" class="section">
+		<div class="container">
+			<h4>Quienes Somos</h4>
+			<div class="row">
+				<div class="span4 offset1">
+					<div>
+						<h2>Somos tu <strong>solución</strong></h2>
+						<p>
+							Somos una ERP que te permite reservar en distintas sociedades y lo unico que necesitas es registrarte
+							como socio y ademas tener muchas ganas de pasar un buen rato con tus amigos o familiares, 
+							por medio de nosotros reservaras el dia y hora que desees desde cualquier lugar.
+							Si eres el dueño de una sociedad gastronomica esta app te permite darle de alta y ponerla a disposcion de reservas.  
+						</p>
+					</div>
+				</div>
+				<div class="span6">
+					<div class="aligncenter">
+						<img src="assets/img/icons/creativity.png" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+	</section>
+	<!-- end spacer section -->
+	<!-- section: contact -->
+	<section id="blog" class="section">
+		<div class="container">
+			<h4>Sociedades populares</h4>
+			<!-- Three columns -->
+			<div class="row">
+				<div class="span3">
+					<div class="home-post">
+						<div class="post-image">
+							<img class="max-img" src="assets/img/blog/img1.jpg" alt="" />
+						</div>
+				
+						<div class="entry-content">
+							<h5><strong><a href="#">Gizartea</a></strong></h5>
+							<p>
+							La que para muchos es conocida como “La Sociedad del Mikelete”, fue fundada en el año 1.932 y se encuentra en la calle San Martin, en la planta sótano nº 15.							</p>
+						</div>
+					</div>
+				</div>
+				<div class="span3">
+					<div class="home-post">
+					<div class="post-image">
+							<img class="max-img" src="assets/img/blog/img2.jpg" alt="" />
+						</div>
+				
+						<div class="entry-content">
+							<h5><strong><a href="#">Basollua</a></strong></h5>
+							<p>
+							Situada en pleno centro de Donostia, los socios y socias de Basollua disfrutamos del placer de estar entre fogones, de una buena mesa y de fantástica compañía							</p>
+						</div>
+					</div>
+				</div>
+				<div class="span3">
+					<div class="home-post">
+					<div class="post-image">
+							<img class="max-img" src="assets/img/blog/img3.jpg" alt="" />
+						</div>
+				
+						<div class="entry-content">
+							<h5><strong><a href="#">Abandotarra K.E.</a></strong></h5>
+							<p>
+							Esta sociedad raliza distintas actividades como la tamborrada en el dia de San Sebastián, salidas montañeras y participación en distintos concursos gastronómicos							</p>
+						</div>
+					</div>
+				</div>
+				<div class="span3">
+					<div class="home-post">
+					<div class="post-image">
+							<img class="max-img" src="assets/img/blog/img4.jpg" alt="" />
+						</div>
+				
+						<div class="entry-content">
+							<h5><strong><a href="#">Aldabe</a></strong></h5>
+							<p>
+							Creada tras la Guerra Civil por varios componentes de otras sociedades desaparecidas, su principal objetivo es el deporte, campo en el que han destacado muchas ocasiones.							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="blankdivider30"></div>
+			<div class="aligncenter">
+				<a href="#" class="btn btn-large btn-theme">Mostrar mas</a>
+			</div>
+		</div>
+	</section>
+	<section id="contact" class="section green">
+		<div class="container">
+			<h4>Contacta con nosotros</h4>
+			<p>
+				Si tienes alguna duda de como mostrar tu sociedad o sobre los requisitos para hacerte socio, puedes contactar con el administrador y cordialmente te contestara.
+			</p>
+			<div class="blankdivider30">
+			</div>
+			<div class="row">
+				<div class="span12">
+					<div class="cform" id="contact-form">
+						<form action="" method="post" role="form" class="contactForm">
+							<div class="row">
+								<div class="span6">
+									<div class="field your-name form-group">
+										<input type="text" name="name" class="form-control" id="name" placeholder="Nombre"/>
+										<div class="validation"></div>
+									</div>
+									<div class="field your-email form-group">
+										<input type="text" class="form-control" name="email" id="email" placeholder="Email" />
+										<div class="validation"></div>
+									</div>
+									<div class="field subject form-group">
+										<input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto"/>
+										<div class="validation"></div>
+									</div>
+								</div>
+								<div class="span6">
+									<div class="field message form-group">
+										<textarea class="form-control" name="message" rows="5" data-rule="required" placeholder="Escribe tu duda o consulta"></textarea>
+										<div class="validation"></div>
+									</div>
+									<input type="submit" value="Enviar Mensaje" class="btn btn-theme pull-left" >
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+				<!-- ./span12 -->
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="span6 offset3">
+					<ul class="social-networks">
+						<li><a href="#"><i class="icon-circled icon-bgdark icon-instagram icon-2x"></i></a></li>
+						<li><a href="#"><i class="icon-circled icon-bgdark icon-twitter icon-2x"></i></a></li>
+						<li><a href="#"><i class="icon-circled icon-bgdark icon-dribbble icon-2x"></i></a></li>
+						<li><a href="#"><i class="icon-circled icon-bgdark icon-pinterest icon-2x"></i></a></li>
+					</ul>
+					<p class="copyright">
+						&copy; GastroSociety. Todos los derechos reservados.
+						<div class="credits">
+							<!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Maxim
+              -->
+							Diseñado por <a href="https://bootstrapmade.com/">Jefry Molina</a>
+						</div>
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- ./container -->
+	</footer>
+	<a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bgdark icon-2x"></i></a>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script src="js/jquery.localScroll.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+	<script src="js/isotope.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/inview.js"></script>
+	<script src="js/animate.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
